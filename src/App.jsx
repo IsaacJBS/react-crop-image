03520bffcbedd0b13f26e096d58005f101995965
Isaac <isaac.jordaobs@gmail.com>
2022-09-02 18:13:14 -0300

    feat: add download image

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,80 @@
-import { useState } from "react";
+import React from "react";
 import "./App.css";
 
-function App() {
+import Cropper from "react-easy-crop";
+import Button from "@mui/material/Button";
+
+import { generateDownload } from "./utils/cropImage";
+
+export default function App() {
+  const inputRef = React.useRef();
+
+  const triggerFileSelectPopup = () => inputRef.current.click();
+
+  const [image, setImage] = React.useState(null);
+  const [croppedArea, setCroppedArea] = React.useState(null);
+  const [crop, setCrop] = React.useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = React.useState(1);
+
+  const onCropComplete = (croppedAreaPercentage, croppedAreaPixels) => {
+    setCroppedArea(croppedAreaPixels);
+  };
+
+  const onSelectFile = (event) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.readAsDataURL(event.target.files[0]);
+      reader.addEventListener("load", () => {
+        setImage(reader.result);
+      });
+    }
+  };
+
+  const onDownload = () => {
+    console.log(generateDownload(image, croppedArea));
+    generateDownload(image, croppedArea);
+  };
+
   return (
-    <div className="App">
-      <h1>Oi</h1>
-      <input type="file" name="photo" id="photo" />
+    <div className="container">
+      <div className="container-cropper">
+        {image ? (
+          <>
+            <div className="cropper">
+              <Cropper
+                image={image}
+                crop={crop}
+                zoom={zoom}
+                aspect={1}
+                onCropChange={setCrop}
+                onZoomChange={setZoom}
+                onCropComplete={onCropComplete}
+              />
+            </div>
+          </>
+        ) : null}
+      </div>
+
+      <div className="container-buttons">
+        <input
+          type="file"
+          accept="image/*"
+          ref={inputRef}
+          onChange={onSelectFile}
+          style={{ display: "none" }}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={triggerFileSelectPopup}
+          style={{ marginRight: "10px" }}
+        >
+          Choose
+        </Button>
+        <Button variant="contained" color="secondary" onClick={onDownload}>
+          Download
+        </Button>
+      </div>
     </div>
   );
 }
-
-export default App;
