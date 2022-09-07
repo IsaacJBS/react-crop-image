1333d3404268f2039ac103d917be3842ad47e5c5
Isaac <isaac.jordaobs@gmail.com>
2022-09-07 12:41:58 -0300

    feat: add landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,29 @@ import "./App.css";
 
 import Filter from "./components/ModalFIlter/Filter";
 
+import nowLogo from "./assets/now-logo.png";
+
 export default function App() {
   return (
     <div className="App">
+      <header className="header">
+        <img className="header-logo" src={nowLogo} alt="" />
+      </header>
       <main className="main">
-        <Filter buttonLabel="Adicionar filtro" />
+        <h2>Gere a sua foto de perfil com o NOW 🤘</h2>
+        <div className="container-filter">
+          <Filter buttonLabel="Clique aqui" />
+          <p>para enviar sua foto</p>
+        </div>
       </main>
+      <section className="how-to-use">
+        <h2>Como utilizar?</h2>
+        <ul>
+          <li>Envie sua foto com o botão acima.</li>
+          <li>Salve a foto.</li>
+          <li>Compartilhe como foto de perfil em suas redes sociais!</li>
+        </ul>
+      </section>
     </div>
   );
 }
